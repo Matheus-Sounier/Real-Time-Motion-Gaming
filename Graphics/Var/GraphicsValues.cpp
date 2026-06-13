6aced390774a1e4a62555f57b9ba776188ca5364
Matheus-Sounier <matheus.sounier2024@gmail.com>
2026-06-13 16:13:33 -0400

    chore: creates a empty MAT 'till preprocessImageAboveLine fill 'em, creates empty vectors getting bigger when createSquare's called

---
+++ Graphics/Var/GraphicsValues.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include "GraphicsValues.hpp"
+
+cv::Mat GraphicsValues::CVMatFrames::img, GraphicsValues::CVMatFrames::imgFlip;
+
+cv::Mat GraphicsValues::CVMatFrames::JumpIMGCrop, GraphicsValues::CVMatFrames::JumpIMGGray, GraphicsValues::CVMatFrames::JumpIMGBackground, GraphicsValues::CVMatFrames::JumpIMGThres, GraphicsValues::CVMatFrames::JumpIMGSub, GraphicsValues::CVMatFrames::JumpIMGDil;
+
+std::vector<GraphicsValues::CVSquares::Images> GraphicsValues::CVSquares::Frames;
+
+std::vector<std::vector<cv::Point>> GraphicsValues::CVSquares::contours;
+std::vector<std::vector<cv::Point>> GraphicsValues::CVJumpLine::mycontours;
+
+std::vector<GraphicsValues::CVSquares::Rectangles> GraphicsValues::CVSquares::Squares;
+
+std::vector<GraphicsValues::CVJumpLine::Line> GraphicsValues::CVJumpLine::Lines;
+
+bool GraphicsValues::CVJumpLine::LineExists = false;
+
+// Minimum contour area to consider
+unsigned GraphicsValues::TOLERANCE = 7;
